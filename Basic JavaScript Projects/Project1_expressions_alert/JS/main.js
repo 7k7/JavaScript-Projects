3e4bbb8a49d27e490215ae7d87734736c1e0cf1e
7k7 <45836591+7k7@users.noreply.github.com>
2024-04-11 18:09:13 -0700

    Added index and main files
    BASIC JAVASCRIPT PROJECTS SUBMISSION ASSIGNMENT

---
+++ Basic JavaScript Projects/Project1_expressions_alert/JS/main.js
@@ -0,0 +1,50 @@
+window.alert('Learning JS with The Tech Academy!'); // prints string in popup text box
+
+document.write('Using document write method to display text'); // prints string in browser's viewport
+
+var A = 'This is a string'; // a string varaible assigment
+
+document.write(A); // prints string in browser's viewport
+
+string = 'string assignment'; // a string varaible assigment (not explict def)
+
+window.alert(string); // prints string in popup text box
+
+// excape character assignment
+
+window.alert('This is so \'cool\''); // prints string in popup text box
+
+// CONCATENATION ASSIGNMENT
+
+document.write("This is " + 'concatenation'); // prints string in browser's viewport
+
+// CONCATENATED STRING CHALLENGE
+
+var concatenatedString = "one " + 'plus ' + 'one'; // variable assignment of three concatenated strings
+document.write(concatenatedString); // prints string in browser's viewport
+
+// MULTIPLE VARIABLES ASSIGNMENT
+
+var family = "the Smiths", son = "David", daughter = "Sally", mom = 'Martha',
+dad = 'John'; // variables declaration and assignment in one statement
+
+document.write(' Its ' + dad + ' from the ' + family + '!'); // prints string in browser's viewport
+
+// ESPRESSION ASSIGNMENT
+
+var onePlusOne = 1 + 1; // variable assignment of an expression
+document.write(onePlusOne); // prints string in browser's viewport
+
+// COMMENT ASSIGNMENT - see above comments
+
+// BASIC JAVASCRIPT PROJECTS SUBMISSION ASSIGNMENT
+
+var Sent1 = "This is the beginning of the string"; //String var assignment of sentence 1
+var Sent2 = " and this is the end of the string"; //String var assignment of sentence 2
+
+var concatenatedSentence = Sent1 + Sent2; //String var assignment by concatenating both Sent1 and Sent2 variables
+
+var expression = 1-1; //Simple expression in the form of a variable assignment
+
+window.alert(Sent1 + Sent2); //pop-up text box displays and concatenates both variables
+document.write(concatenatedSentence); //diplays concatenated variable of two string vars in the broswer's viewport
